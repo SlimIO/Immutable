d65785c55080ce4504d232c0c6b17cffcf2007e2
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-16 19:35:27 +0100

    chore: add missing freezedProperty in d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 declare namespace Immutable {
     export function seal<T extends object>(target: T): T;
+    export function freezedProperty(target: object, propertyKey: symbol | string, value?: any): void;
 }
 
 export as namespace Immutable;

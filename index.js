ad47c01d264c9554d941a89311573420d6b89a89
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-16 19:27:29 +0100

    feat: add freezedProperty func

--- index.js
+++ index.js
@@ -51,4 +51,32 @@ function seal(target) {
     });
 }
 
-module.exports = { seal };
+/**
+ * @func freezedProperty
+ * @desc Define a private (non-enumable, non-configurable) property on the target
+ * @param {!Object} target target object
+ * @param {!String | Symbol} propertyKey The name of the property we want to define in target
+ * @param {*} [value=null] The property value
+ * @returns {void}
+ *
+ * @throws {Error}
+ * @throws {TypeError}
+ */
+function freezedProperty(target, propertyKey, value = null) {
+    if (!is.symbol(propertyKey) && !is.string(propertyKey)) {
+        throw new TypeError("propertyKey must be a symbol or a string");
+    }
+
+    const ok = Reflect.defineProperty(target, propertyKey, {
+        value,
+        enumerable: false,
+        configurable: false,
+        writable: false
+    });
+
+    if (!ok) {
+        throw new Error(`Unable to define propertyKey ${propertyKey} on target ${getObjectType(target)}`);
+    }
+}
+
+module.exports = { seal, freezedProperty };
